103637c5587e98a1f82d0330c526b4e47c27561b
aspirin100 <aspi.jb.ru@gmail.com>
2025-12-26 14:38:32 +0300

    app init in main and exception handling

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,18 @@
 
 int main()
 {
-    Config config;
-
     try
     {
-        auto conf = ParseConfig();
-        if (!conf)
-        {
-            std::cerr << "failed to read config" << std::endl;
-            return -1;
-        }
+        auto config = ParseConfig().value();
 
-        config = std::move(conf.value()); // TODO: rule of 5
-    }
-    catch (const std::exception &ex)
-    {
-        std::cout << ex.what() << std::endl;
-    }
-
-    try
-    {
         Application app(config);
     }
     catch (const std::exception &ex)
     {
         std::cout << ex.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-    return 0;
+
+    return EXIT_SUCCESS;
 }
